81b67bba54a2ed0b3df3444caff05245b35153c0
dj-pocket-cloud <mstevens4@umassd.edu>
2019-04-07 18:50:12 -0400

    added functionality to some more buttons

--- app/src/main/java/rs/example/audioplayer/ControlsFragment.java
+++ app/src/main/java/rs/example/audioplayer/ControlsFragment.java
@@ -19,6 +19,10 @@ public class ControlsFragment extends Fragment {
     String uriString = "android.resource://rs.example.audioplayer/" + R.raw.knux;
 
     ImageButton playButton;
+    ImageButton rewindButton;
+    ImageButton loopButton;
+
+    int loop = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,21 +39,41 @@ public class ControlsFragment extends Fragment {
         }
 
         playButton = (ImageButton)view.findViewById(R.id.play);
-        playButton.setOnClickListener(clickListener);
+        rewindButton = (ImageButton)view.findViewById(R.id.rewind);
+        loopButton = (ImageButton)view.findViewById(R.id.loopButton);
+        playButton.setOnClickListener(playButtonListener);
+        rewindButton.setOnClickListener(rewindButtonListener);
+        loopButton.setOnClickListener(loopButtonListener);
         return view;
 
     }
 
-    OnClickListener clickListener = new OnClickListener() {
+    //play the currently loaded song when this button is pressed
+    OnClickListener playButtonListener = new OnClickListener() {
         @Override
         public void onClick(View v) {
-
             try {
                 operations.toggleMusic();
             } catch (RemoteException e) {
                 e.printStackTrace();
             }
 
+        }
+    };
+
+    //seek to beginning of track
+    OnClickListener rewindButtonListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //TODO: replace this with playFromPoint(beginningOfSnippet) when audio snippets are implemented
+            operations.seekBack();
+        }
+    };
+
+    OnClickListener loopButtonListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            operations.setLooping((loop++) % 2 == 1);
         }
     };
 
--- app/src/main/java/rs/example/audioplayer/TrackOperations.java
+++ app/src/main/java/rs/example/audioplayer/TrackOperations.java
@@ -16,15 +16,39 @@ public class TrackOperations {
     }
 
     public void setSource(Context context, Uri uri) throws IOException {
+        player.stop();
         player.setDataSource(context, uri);
         player.prepare();
     }
 
     public void toggleMusic() throws RemoteException {
-        if (player.isPlaying()) {
-            player.pause();
-        } else {
-            player.start();
-        }
+        //play or pause the player, depending on playing status
+        if (player.isPlaying()) { player.pause(); }
+        else { player.start(); }
+    }
+
+    public void seekBack() {
+        //get the current status of the player and store it in a local boolean
+        boolean wasPlaying = player.isPlaying();
+
+        //stop the music, and if it was playing then start playing again
+        player.pause();
+        player.seekTo(0);
+        if (wasPlaying) { player.start(); }
+    }
+
+    public void playFromPoint(int msec) {
+        player.pause();
+        player.seekTo(msec);
+        player.start();
+    }
+
+    //set looping status
+    public void setLooping(boolean loop) {
+        player.setLooping(loop);
+    }
+
+    public int getPosition() {
+        return player.getCurrentPosition(); //returns in milliseconds
     }
 }
